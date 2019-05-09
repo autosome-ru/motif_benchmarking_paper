4e771ba59d617a626f2395e1c8b3270da1b3db31
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-05-09 10:40:55 +0300

    motif families/classes added

---
+++ motif_families.rb
@@ -0,0 +1,48 @@
+require 'json'
+TFCLASS_LEVELS = [:tf_superclass, :tf_class, :tf_family, :tf_subfamily, :tf_genus, :tf_molecular_species]
+
+tfclass_level = Integer(ARGV[0])
+tfclass_level_name = TFCLASS_LEVELS[tfclass_level - 1]
+
+motif_tfs = [
+  'source_data/hocomoco_genes2mat.txt',
+  'source_data/jaspar_genes2mat.txt',
+].flat_map{|fn|
+  File.readlines(fn).map{|l|
+    l.split("\t").map(&:strip)
+  }
+}.group_by{|tf, motif|
+  motif
+}.map{|motif, pairs|
+  [motif, pairs.map(&:first)]
+}
+
+tf_infos = File.readlines('all_tf_infos.json').map{|l|
+  JSON.parse(l, symbolize_names: true)
+}
+
+tf_info_by_name = tf_infos.group_by{|info|
+  info[:tf_gene_name]
+}.map{|k,vs|
+  [k, vs.first]
+}.to_h
+
+motif_families = motif_tfs.map{|motif, tfs|
+  fams = tfs.map{|tf|
+    tf_info_by_name[tf]
+  }.flat_map{|info|
+    info[tfclass_level_name]
+  }.uniq
+  [motif, fams]
+}
+
+motif_families.each{|motif, fams|
+  if fams.size == 1
+    fam = fams.first
+  elsif fams.size == 0
+    fam = 'unknown'
+  elsif fams.size > 1
+    fam = 'ambiguous'
+  end
+  puts [motif, fam].join("\t")
+}
