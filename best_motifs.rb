e20d8ff00c2876360a383ffaffe8b561d16c126b
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2019-05-11 00:34:05 +0300

    extract aucs matrix loading into a separate file

--- best_motifs.rb
+++ best_motifs.rb
@@ -1,4 +1,5 @@
 require 'json'
+require_relative 'aucs'
 TFCLASS_LEVELS = [:tf_superclass, :tf_class, :tf_family, :tf_subfamily, :tf_genus, :tf_molecular_species]
 
 class Array
@@ -72,29 +73,6 @@ end
 
 def motif_tf(motif)
   TF_BY_MOTIF[motif]
-end
-
-class Aucs
-  attr_reader :experiments, :aucs_by_motif
-  def initialize(experiments, aucs_by_motif)
-    @experiments = experiments
-    @experiment_index = @experiments.each_with_index.to_h
-    @aucs_by_motif = aucs_by_motif
-  end
-  def motifs; @aucs_by_motif.keys; end
-  def auc(motif, experiment); @aucs_by_motif[motif][ @experiment_index[experiment] ]; end
-  def motif_aucs(motif); @experiments.zip(@aucs_by_motif[motif]).to_h; end
-  def experiment_aucs(experiment); motifs.map{|motif| [motif, auc(motif, experiment)] }.to_h; end
-  def self.from_file(fn)
-    File.open(fn){|f|
-      experiments = f.readline.chomp.split("\t") # yes, there is no empty cell in the left top corner
-      aucs_by_motif = f.readlines.map{|l|
-        motif, *aucs = l.chomp.split("\t")
-        [motif, aucs.map(&:to_f)]
-      }.to_h
-      self.new(experiments, aucs_by_motif)
-    }
-  end
 end
 
 aucs = Aucs.from_file(aucs_matrix_fn)
